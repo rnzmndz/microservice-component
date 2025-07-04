91f08c03e6b027b350e80c3276d08b4b293ef6c7
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-07-04 21:26:43 +0800

    update

--- api-gateway/src/main/java/com/renzo/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/renzo/api_gateway/config/SecurityConfig.java
@@ -1,24 +1,28 @@
 package com.renzo.api_gateway.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
-//@Configuration
+@Configuration
 public class SecurityConfig {
 
+//    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+//    private String jwtIssuerUri;
+
 //    @Bean
-//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
 //        http
-//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-//                .authorizeExchange(exchange -> exchange
-//                        .pathMatchers("/auth/**").permitAll()  // allow login requests
-//                        .anyExchange().authenticated()         // all other endpoints must be secured
-//                );
-////                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-//
+//                .authorizeExchange(exchanges -> exchanges
+//                        .pathMatchers("/public/**").permitAll()
+//                        .anyExchange().authenticated()
+//                )
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 //        return http.build();
+//    }
+
+
+//    @Bean
+//    public ReactiveJwtDecoder jwtDecoder() {
+//        // Use ReactiveJwtDecoders for reactive applications
+//        return ReactiveJwtDecoders.fromIssuerLocation(jwtIssuerUri);
 //    }
 }
