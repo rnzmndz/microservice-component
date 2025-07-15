95953730d0555756e45ac4f86cdac09a40e90a72
Renzo Mendoza <renzomendoza.me@gmail.com>
2025-07-15 16:42:18 +0800

    update

--- api-gateway/src/main/java/com/renzo/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/renzo/api_gateway/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.renzo.api_gateway.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
@@ -22,6 +23,7 @@ public class SecurityConfig {
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/public/**").permitAll()
                         .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/employee-service/v3/api-docs").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
